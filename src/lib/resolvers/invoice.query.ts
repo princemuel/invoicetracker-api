b511b8b329f636cf051c08a431a789ee314f3bef
Prince Muel <vansomecsam@gmail.com>
2023-01-16 13:54:26 +0100

    feat: add graphql query types
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/lib/resolvers/invoice.query.ts
@@ -0,0 +1,23 @@
+import { list, nonNull, nullable, queryField } from "nexus";
+import { ItemWhereUniqueInput } from "./invoice.input";
+
+export const invoices = queryField("invoices", {
+  type: nonNull(list(nonNull("Invoice"))),
+  resolve: async (root, args, ctx) => {
+    return ctx.db.invoice.findMany({});
+  },
+});
+
+export const invoice = queryField("invoice", {
+  type: nullable("Invoice"),
+  args: {
+    where: nonNull(ItemWhereUniqueInput),
+  },
+  resolve: async (root, args, ctx) => {
+    return ctx.db.invoice.findUniqueOrThrow({
+      where: {
+        id: args.where.id,
+      },
+    });
+  },
+});
