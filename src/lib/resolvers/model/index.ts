2e4201d930ec4438300b4dbc16ed7a6bba7ab6ad
Prince Muel <vansomecsam@gmail.com>
2023-01-18 17:19:43 +0100

    feat: rename nexus models
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/lib/resolvers/model/index.ts
@@ -0,0 +1,3 @@
+export * from './invoice';
+export * from './other';
+export * from './user';
---
+++ src/lib/resolvers/model/user.ts
@@ -0,0 +1,35 @@
+import { enumType, nonNull, objectType } from "nexus";
+
+export const User = objectType({
+  name: "User",
+  definition(t) {
+    t.nonNull.id("id", { description: "Id of the user" });
+    t.nonNull.datetime("createdAt", {
+      description: `The exact time the user was created`,
+    });
+    t.datetime("updatedAt", {
+      description: `The exact time the user was updated`,
+    });
+    t.string("firstName", { description: "The first name of the user" });
+    t.string("lastName", { description: "The last name of the user" });
+    t.nonNull.string("email", { description: "The password of the user" });
+    t.string("password", { description: "The password of the user" });
+    t.string("photo", { description: "The avatar of the user" });
+  },
+});
+
+export const AuthPayload = objectType({
+  name: "AuthPayload",
+  definition(t) {
+    t.nonNull.field("user", {
+      type: nonNull(User),
+    });
+    t.nonNull.string("accessToken");
+  },
+});
+
+export const Role = enumType({
+  name: "Role",
+  description: "The current role of the user",
+  members: ["USER", "ADMIN"],
+});
