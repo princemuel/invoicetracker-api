d87b812a38e8ba1695fef78240bff86adb2ea256
Prince Muel <vansomecsam@gmail.com>
2023-01-18 17:36:43 +0100

    feat: setup the app's bootstrap scripts
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,86 @@
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerErrorCode } from '@apollo/server/errors';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} from '@apollo/server/plugin/landingPage/default';
+import { ApolloServerPluginUsageReporting } from '@apollo/server/plugin/usageReporting';
+import { json } from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import { createContext, schema } from './lib';
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+const localOrigin = [/^http:\/\/localhost:\d{4}$/];
+const prodOrigin = [/^https:\/\/.*\.yourdomain\.com$/];
+const PORT = process.env.PORT || 4000;
+
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginUsageReporting({
+        // If you pass unmodified: true to the usage reporting
+        // plugin, Apollo Studio receives ALL error details
+        sendErrors: { unmodified: true },
+      }),
+      // Install a landing page plugin based on NODE_ENV
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageProductionDefault({
+            // graphRef: 'my-graph-id@my-graph-variant',
+            footer: false,
+            includeCookies: true,
+          })
+        : ApolloServerPluginLandingPageLocalDefault({
+            footer: false,
+            includeCookies: true,
+          }),
+    ],
+    formatError: (formattedError, error) => {
+      // Return a different error message
+      if (
+        formattedError?.extensions?.code ===
+        ApolloServerErrorCode.GRAPHQL_VALIDATION_FAILED
+      ) {
+        return {
+          ...formattedError,
+          message:
+            "Your query doesn't match the schema. Try double-checking it!",
+        };
+      }
+
+      // Otherwise return the formatted error. This error can also
+      // be manipulated in other ways, as long as it's returned.
+      return formattedError;
+    },
+  });
+  await server.start();
+
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>({
+      origin: IS_DEV ? localOrigin : prodOrigin,
+      credentials: true,
+    }),
+    json(),
+    cookieParser(),
+    expressMiddleware(server, {
+      context: createContext,
+    })
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+}
+
+// Start server
+startApolloServer();
