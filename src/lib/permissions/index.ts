bccc1752b8001399a082909b333937c73f50362c
Prince Muel <vansomecsam@gmail.com>
2023-01-25 17:18:34 +0100

    feat: setup route permissions
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/permissions/index.ts
+++ src/lib/permissions/index.ts
@@ -1,4 +1,3 @@
-import { ApolloServerErrorCode } from '@apollo/server/errors';
 import { GraphQLError } from 'graphql';
 import { allow, shield } from 'graphql-shield';
 import { rules } from './rules';
@@ -6,40 +5,39 @@ import { rules } from './rules';
 export const permissions = shield(
   {
     Query: {
-      // "*": isAuthenticated,
+      '*': rules.isAuthenticated,
     },
     Mutation: {
-      // "*": isAuthenticated,
-      createInvoice: rules.isAuthenticated,
-      deleteInvoice: rules.isAuthenticated,
+      '*': rules.isAuthenticated,
+      login: allow,
+      register: allow,
+      refreshAuth: allow,
+      logout: allow,
     },
   },
   {
     fallbackRule: allow,
-
-    async fallbackError(error, root, args, context, info) {
-      if (
-        error.extensions?.code === ApolloServerErrorCode.GRAPHQL_PARSE_FAILED
-      );
-      if (error instanceof ApolloError) {
+    async fallbackError(error, root, args, ctx, info) {
+      if (error instanceof GraphQLError) {
         // expected errors
         return error;
       }
       if (error instanceof Error) {
         // unexpected errors
         console.error(error);
-        return new GraphQLError(
-          'Internal server error',
-          'ERR_INTERNAL_SERVER_ERROR'
-        );
+        return new GraphQLError('Error: Internal server error', {
+          extensions: {
+            code: 'ERR_INTERNAL_SERVER_ERROR',
+            message: error.message,
+          },
+        });
       }
       // what the hell got thrown
       console.error('The resolver threw something that is not an error.');
       console.error(error);
-      return new GraphQLError(
-        'Internal server error',
-        'ERR_INTERNAL_SERVER_UNKWOWN'
-      );
+      return new GraphQLError('Unknown: Internal server error', {
+        extensions: { code: 'ERR_INTERNAL_SERVER_UNKWOWN' },
+      });
     },
   }
 );
--- src/lib/permissions/rules.ts
+++ src/lib/permissions/rules.ts
@@ -1,19 +1,10 @@
 import { rule } from 'graphql-shield';
 import { Context } from '../context';
-import { getUserId } from '../utils';
 
 export const rules = {
   isAuthenticated: rule({ cache: 'contextual' })(
-    async (root, args, ctx: Context, info) => {
-      const userId = getUserId(ctx);
-
-      const user = await ctx.db.user.findFirstOrThrow({
-        where: {
-          id: userId || undefined,
-        },
-      });
-
-      return user != null;
+    async (_root, _args, ctx: Context, _info) => {
+      return ctx.user !== null;
     }
   ),
 };
