fdf2a2636064953366ff0559cafbcdd31a7bd11c
Prince Muel <vansomecsam@gmail.com>
2023-01-27 03:33:58 +0100

    feat: update environment variables
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/utils/environment.ts
+++ src/lib/utils/environment.ts
@@ -1,17 +1,17 @@
-import * as fs from 'fs';
-import * as path from 'path';
-
-const readFile = (filePath: string) => {
-  return fs.readFileSync(path.join(process.cwd(), filePath), 'utf8');
-};
+require('dotenv').config();
+import { readFile } from './parsers';
 
 export const constants = {
-  JWT_ACCESS_SECRET: readFile('private-at.pem'),
   JWT_ACCESS_PUBLIC: readFile('public-at.pem'),
-  JWT_REFRESH_SECRET: readFile('private-rt.pem'),
   JWT_REFRESH_PUBLIC: readFile('public-rt.pem'),
+
+  JWT_ACCESS_SECRET: readFile('private-at.pem'),
+  JWT_REFRESH_SECRET: readFile('private-rt.pem'),
+
   JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION || '15',
   JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION || '1',
+
+  PORT: process.env.PORT || 4000,
 };
 
 // console.log(Object.values(constants).map((value) => value));
