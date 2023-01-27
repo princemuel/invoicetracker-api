18c68d6590ff12b8476b7b2f225a820e1c3c26e7
Prince Muel <vansomecsam@gmail.com>
2023-01-27 20:12:05 +0100

    feat: add environment variables to config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ src/config/environment.ts
@@ -0,0 +1,17 @@
+require('dotenv').config();
+import { readFile } from '../lib/utils/files';
+
+export const constants = {
+  JWT_ACCESS_PUBLIC: readFile('public-at.pem'),
+  JWT_REFRESH_PUBLIC: readFile('public-rt.pem'),
+
+  JWT_ACCESS_SECRET: readFile('private-at.pem'),
+  JWT_REFRESH_SECRET: readFile('private-rt.pem'),
+
+  JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION || '15',
+  JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION || '1',
+
+  PORT: process.env.PORT || 4000,
+};
+
+// console.log(Object.values(constants).map((value) => value));
