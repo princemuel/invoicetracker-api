590c6b4eb35d639ae3e54e6e397914b115165401
Prince Muel <vansomecsam@gmail.com>
2023-01-28 19:10:59 +0100

    chore: cleanup (create user context imports)
    References: #13
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/permissions/rules.ts
+++ src/lib/permissions/rules.ts
@@ -1,12 +1,10 @@
 import { rule } from 'graphql-shield';
 import { Context } from '../context';
-import { createUserContext } from '../utils';
 
 export const rules = {
   isAuthenticated: rule({ cache: 'contextual' })(
     async (_root, _args, ctx: Context, _info) => {
-      const { req } = ctx;
-      const user = await createUserContext(req);
+      const user = await ctx.createUserContext(ctx.req);
       return user !== null;
     }
   ),
