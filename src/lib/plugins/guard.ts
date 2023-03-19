94d1ce73e2431bc3c02eea6047649088da8d487c
Prince Muel <vansomecsam@gmail.com>
2023-03-19 09:24:54 +0100

    style: format files with prettier
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/plugins/guard.ts
+++ src/lib/plugins/guard.ts
@@ -1,4 +1,4 @@
-import { nullabilityGuardPlugin } from "nexus";
+import { nullabilityGuardPlugin } from 'nexus';
 
 export const guardPlugin = nullabilityGuardPlugin({
   // @ts-expect-error
@@ -7,7 +7,7 @@ export const guardPlugin = nullabilityGuardPlugin({
     console.error(
       `Error: Saw a null value for non-null field ${info.parentType.name}.${
         info.fieldName
-      } ${root ? `(${root.id || root._id})` : ""}`
+      } ${root ? `(${root.id || root._id})` : ''}`
     );
   },
   // A map of `typeNames` to the values we want to replace with if a "null" value
@@ -15,7 +15,7 @@ export const guardPlugin = nullabilityGuardPlugin({
   // for the `objectType` / `enumType` definition, as seen below.
   fallbackValues: {
     Int: () => 0,
-    String: () => "",
+    String: () => '',
     ID: ({ info }) => `${info.parentType.name}:N/A`,
     Boolean: () => false,
     Float: () => 0,
