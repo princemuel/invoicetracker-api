a15d9a1f63e3c5f7cbd83538efa45c477bba5beb
Prince Muel <vansomecsam@gmail.com>
2023-03-19 09:31:49 +0100

    ci: add more rules to eslint configuration
    [Ticket: X]
    
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,24 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.eslint.json'],
   },
   plugins: ['@typescript-eslint'],
+  ignorePatterns: ['.eslintrc.cjs'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
   ],
+  rules: {
+    // Note: you must disable the base rule as it can report incorrect errors
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'off',
+  },
 };
