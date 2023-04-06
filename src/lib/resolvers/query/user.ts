614952969b96837a17b6e48f957639136af0c879
Prince Muel <vansomecsam@gmail.com>
2023-04-06 18:45:29 +0100

    refactor: change refresh mutation to query
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/resolvers/query/user.ts
+++ src/lib/resolvers/query/user.ts
@@ -1,8 +1,36 @@
+import { GraphQLError } from 'graphql';
 import { nullable, queryField } from 'nexus';
+import { createTokens, getRefreshCookie } from 'src/lib/utils';
 
 export const user = queryField('user', {
   type: nullable('User'),
   resolve: async (root, args, ctx) => {
     return await ctx.createUserContext(ctx.req);
+  },
+});
+
+export const refreshAuth = queryField('refreshAuth', {
+  type: 'RefreshPayload',
+  resolve: async (_root, _args, ctx) => {
+    const decoded = getRefreshCookie(ctx);
+    const user = await ctx.db.user.findUnique({
+      where: {
+        id: decoded.user,
+      },
+    });
+
+    const message = 'Invalid user: This user was not found';
+    if (!user)
+      throw new GraphQLError(message, {
+        extensions: {
+          code: 'FORBIDDEN',
+          http: { status: 403 },
+        },
+      });
+
+    const { accessToken } = createTokens({ user: user.id }, ctx);
+    return {
+      accessToken,
+    };
   },
 });
