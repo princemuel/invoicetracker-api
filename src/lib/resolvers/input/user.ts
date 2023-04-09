edfb685a92d58c665c95a98495ed0760f8d305f7
Prince Muel <vansomecsam@gmail.com>
2023-04-09 10:15:27 +0100

    feat: update graphql input schema for user
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/resolvers/input/user.ts
+++ src/lib/resolvers/input/user.ts
@@ -3,9 +3,7 @@ import { inputObjectType } from 'nexus';
 export const RegisterInput = inputObjectType({
   name: 'RegisterInput',
   definition(t) {
-    t.nonNull.string('firstName');
-    t.nonNull.string('lastName');
-    t.nonNull.string('email');
+    t.nonNull.string('email', { description: 'The email of the user' });
     t.nonNull.string('password', {
       description:
         'The password of the user. Must match the countersign i.e the reentered password',
@@ -19,7 +17,9 @@ export const RegisterInput = inputObjectType({
 export const LoginInput = inputObjectType({
   name: 'LoginInput',
   definition(t) {
-    t.nonNull.string('email');
-    t.nonNull.string('password');
+    t.nonNull.string('email', { description: 'The email of the user' });
+    t.nonNull.string('password', {
+      description: 'The password of the user',
+    });
   },
 });
