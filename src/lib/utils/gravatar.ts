185b284ba91547fcc2943918d18299a3f887ec01
Prince Muel <vansomecsam@gmail.com>
2023-04-09 10:39:32 +0100

    feat: update gravatar function
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/lib/utils/gravatar.ts
+++ src/lib/utils/gravatar.ts
@@ -4,8 +4,11 @@ export function gravatar(email = '', size = 200, defaults = 'retro') {
   const BASE_URL = `https://gravatar.com/avatar`;
 
   if (!email) return `${BASE_URL}/?s=${size}&d=${defaults}`;
-  email = email.trim();
 
-  const hash = crypto.createHash('md5').update(email).digest('hex').toString();
+  const hash = crypto
+    .createHash('md5')
+    .update(email.trim())
+    .digest('hex')
+    .toString();
   return `${BASE_URL}/${hash}?s=${size}&d=${defaults}`;
 }
