d72e67724f801c93d1b2adae48f44031e9338216
Prince Muel <vansomecsam@gmail.com>
2023-04-26 16:23:34 +0100

    feat: update env variables
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,22 +1,39 @@
-import * as dotenv from 'dotenv-safe';
 import { readFile } from '../lib/utils/files';
 
-dotenv.config();
-
 export const constants = {
-  JWT_PUBLIC_KEY: readFile('public.pem'),
-  JWT_PRIVATE_KEY: readFile('private.pem'),
-
-  JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION || '15',
-  JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION || '3',
-
+  IS_DEV: process.env.NODE_ENV === 'development',
+  IS_PROD: process.env.NODE_ENV === 'production',
   PORT: process.env.PORT || 4000,
-  BASE_URL: process.env.BASE_URL || '',
-
+  SERVER_URL: process.env.SERVER_URL || '',
   SMTP: {
     HOST: 'EMAIL_HOST',
     PASS: 'EMAIL_PASS',
     PORT: 'EMAIL_PORT',
     USER: 'EMAIL_USER',
   },
+  JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION || '15',
+  JWT_REFRESH_EXPIRATION: process.env.JWT_REFRESH_EXPIRATION || '3',
+
+  JWT_PUBLIC_KEY: readFile('public.pem'),
+  JWT_PRIVATE_KEY: readFile('private.pem'),
+
+  // JWT_PUBLIC_KEY:
+  //   process.env.NODE_ENV === 'development'
+  //     ? readFile('public.pem')
+  //     : parseBuffer(process.env.JWT_PUBLIC_KEY!, 'base64', 'ascii'),
+  // JWT_PRIVATE_KEY:
+  //   process.env.NODE_ENV === 'development'
+  //     ? readFile('private.pem')
+  //     : parseBuffer(process.env.JWT_PRIVATE_KEY!, 'base64', 'ascii'),
 } as const;
+
+// console.log(
+//   Object.entries(constants).map(([key, value]) => {
+//     return { key, value };
+//   })
+// );
+
+// console.log({
+//   PRIVATE_KEY: Buffer.from(readFile('private.pem')).toString('base64'),
+//   PUBLIC_KEY: Buffer.from(readFile('public.pem')).toString('base64'),
+// });
