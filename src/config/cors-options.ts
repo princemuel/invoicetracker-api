ac5df8cdf9d0fa057be886571d1fa6dd88785345
Prince Muel <vansomecsam@gmail.com>
2023-04-27 23:26:58 +0100

    feat: update cors options
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- src/config/cors-options.ts
+++ src/config/cors-options.ts
@@ -1,26 +1,26 @@
 import cors from 'cors';
-import { constants } from './environment';
 
-const local = [/^http:\/\/localhost:\d{4}$/];
+// const local = [/^http:\/\/localhost:\d{4}$/];
 // const prod = [/^https:\/\/.*\.yourdomain\.com$/];
-const prod = constants.BASE_URL;
+// const prod = constants.SERVER_URL;
+
+const allowedOrigins = [
+  'https://pm-invoice.vercel.app/',
+  'https://studio.apollographql.com',
+  'http://localhost:4000',
+  'http://localhost:3000',
+];
 
 // Cross Orign Resource Sharing
 export const corsOptions = {
-  // origin: (origin, callback) => {
-  //   if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
-  // origin: isDev ? local : prod,
-  origin: [
-    'https://studio.apollographql.com',
-    'http://localhost:8000',
-    'http://localhost:4000',
-    'http://localhost:3000',
-  ],
-  // optionsSuccessStatus: 200,
+  origin: (origin, callback) => {
+    // disallow origin for postman and other origin later
+    if (allowedOrigins.includes(origin!) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
+  optionsSuccessStatus: 200,
 } as cors.CorsOptions;
